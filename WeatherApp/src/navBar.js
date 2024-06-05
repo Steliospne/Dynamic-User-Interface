075345c88b84f135d9dc0a0e25d7a76bbd1023a5
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-06-05 20:25:05 +0200

    Adds functionality that shares the state of the menu to its childeren and also now involves a search bar

--- WeatherApp/src/navBar.js
+++ WeatherApp/src/navBar.js
@@ -1,3 +1,5 @@
+import Views from "./views";
+
 export default class NavBar {
   static create() {
     const nav = document.createElement("div");
@@ -18,15 +20,25 @@ export default class NavBar {
 
     let menu_state = 0;
     menuButtonEl.addEventListener("click", buttonHandler);
+
     function buttonHandler() {
       const menu = document.querySelector(".menu");
       menu_state = menu_state == 0 ? 1 : 0;
+
       if (menu_state) {
         menu.classList.add("on");
         menu.classList.remove("off");
+        menu.childNodes.forEach((child) => {
+          child.classList.add("on");
+          child.classList.remove("off");
+        });
       } else {
         menu.classList.remove("on");
         menu.classList.add("off");
+        menu.childNodes.forEach((child) => {
+          child.classList.remove("on");
+          child.classList.add("off");
+        });
       }
     }
 
@@ -35,7 +47,24 @@ export default class NavBar {
 
   static createMenu() {
     const menu = document.createElement("div");
+    const smallViews = Views.getSmallViews();
+    const searchWrapper = document.createElement("div");
+    const inputCity = document.createElement("input");
+    const searchButton = document.createElement("button");
+
+    searchWrapper.className = "search-wrapper off";
+    inputCity.id = "city";
+    inputCity.name = "city";
+    inputCity.placeholder = "Search for city or town"
+    searchWrapper.append(inputCity, searchButton)
+    menu.append(searchWrapper);
+
+    smallViews.forEach((view) => {
+      menu.append(view);
+    });
     menu.className = "menu off";
+
+
     return menu;
   }
 }
