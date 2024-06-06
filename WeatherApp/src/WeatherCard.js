697f8fb04e06dffe7ae99ff15d4ce8f8fed64d15
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-06-06 18:13:02 +0200

    Seperates the create function to create and create header also adds a constructor for individual cards that are created by the user

--- WeatherApp/src/WeatherCard.js
+++ WeatherApp/src/WeatherCard.js
@@ -1,37 +1,65 @@
 export default class WeatherCard {
-  static set(locationData, weatherData) {
-    WeatherCard.locationData = locationData;
-    WeatherCard.weatherData = weatherData;
+  constructor() {
+    this.id;
+    this.locationData = "";
+    this.weatherData = "";
+    this.bigView = WeatherCard.create();
+    this.smallView = WeatherCard.createHeader();
+    this.location = "";
+  }
 
-    document.querySelector(".city-field").textContent = WeatherCard.locationData.city;
-    document.querySelector(".tempStatus-field").innerHTML = `${WeatherCard.weatherData.current.temp_c} &deg C | ${WeatherCard.weatherData.current.condition.text}`;
+  set(locationData, weatherData) {
+    this.locationData = locationData;
+    this.weatherData = weatherData;
+
+    this.bigView.firstChild.childNodes[1].textContent = this.locationData.city;
+    this.bigView.firstChild.childNodes[2].innerHTML = `${this.weatherData.current.temp_c} &deg C | ${this.weatherData.current.condition.text}`;
+
+    this.smallView.childNodes[1].textContent = this.locationData.city;
+    this.smallView.childNodes[2].innerHTML = `${this.weatherData.current.temp_c} &deg C | ${this.weatherData.current.condition.text}`;
+  }
+
+  setLocation(location) {
+    this.location = location;
+    this.smallView.childNodes[0].textContent = this.location;
+  }
+
+  getBigView() {
+    return this.bigView;
+  }
+
+  getSmallView() {
+    return this.smallView;
+  }
+
+  render(view) {
+    document.body.append(view);
   }
 
   static update() {}
 
   static create() {
     const cardContainer = document.createElement("div");
+    const cardHeader = WeatherCard.createHeader();
+    cardContainer.className = "card-container";
+    cardContainer.append(cardHeader);
+
+    return cardContainer;
+  }
+
+  static createHeader() {
     const cardHeader = document.createElement("div");
     const locationField = document.createElement("div");
     const cityField = document.createElement("div");
     const tempStatusField = document.createElement("div");
-
-    cardContainer.className = "card-container";
     cardHeader.className = "card-header";
     locationField.className = "location-field";
     cityField.className = "city-field";
     tempStatusField.className = "tempStatus-field";
 
     locationField.textContent = "My Location";
-
     cardHeader.append(locationField, cityField, tempStatusField);
-    cardContainer.append(cardHeader);
-    document.body.append(cardContainer);
 
-    return cardContainer
-  }
-
-  static getHeader() {
-    return document.querySelector(".card-header").cloneNode(true);
+    return cardHeader;
   }
 }
