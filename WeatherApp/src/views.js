072423a76cb8fa108b84a4d933a475eb203763cc
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-06-06 18:10:26 +0200

    Adds the module views to handle the different cards with ability to store the session state in the future

--- WeatherApp/src/views.js
+++ WeatherApp/src/views.js
@@ -3,15 +3,52 @@ import WeatherCard from "./WeatherCard";
 export default class Views {
   static small = [];
   static big = [];
+  static counterBig = 0;
+  static counterSmall = 0;
 
   static init() {
-    Views.small.push(WeatherCard.getHeader());
+    const initialView = new WeatherCard();
+    const displayContainer = document.createElement("div");
+    displayContainer.className = "display-container";
+    document.body.append(displayContainer);
+
+    Views.big.push(initialView);
+    Views.big.forEach((obj) => {
+      obj.id = Views.counterBig;
+      obj.bigView.id = Views.counterBig;
+      Views.counterBig++;
+      Views.render(obj.bigView);
+    });
+
+    Views.small.push(initialView.getSmallView());
     Views.small.forEach((view) => {
+      view.id = Views.counterSmall;
       view.className = "small-view off";
+      Views.counterSmall++;
     });
   }
 
   static getSmallViews() {
     return Views.small;
+  }
+
+  static setView(view) {
+    Views.big.push(view);
+    Views.big.forEach((obj) => {
+      obj.id = Views.counterBig;
+      obj.bigView.id = Views.counterBig;
+      Views.counterBig++;
+      Views.render(obj.bigView);
+    });
+    Views.small.push(view.getSmallView());
+    Views.small.forEach((view) => {
+      view.className = "small-view on";
+    });
+  }
+
+  static render(view) {
+    const displayContainer = document.querySelector(".display-container")
+    // displayContainer.innerHTML = "";
+    displayContainer.append(view);
   }
 }
